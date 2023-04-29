1ac6107319562859597704235da83387367b02fa
rajendrawings <rajendra.badgujar@integrationwings.com>
2023-04-29 17:32:31 +0530

    Added Migration commands

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -3,3 +3,7 @@
 virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
 pip install -r /home/ubuntu/iwinghire/requirements.txt
+
+python /home/ubuntu/iwinghire/manage.py makemigrations
+python /home/ubuntu/iwinghire/manage.py migrate
+python /home/ubuntu/iwinghire/manage.py makemigrations
