f40d34f172f6afcbb375ec0cd9f649853932c2b8
rajendrawings <rajendra.badgujar@integrationwings.com>
2023-04-30 00:31:14 +0530

    M1

--- iwinghire/settings.py
+++ iwinghire/settings.py
@@ -73,12 +73,35 @@ WSGI_APPLICATION = 'iwinghire.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'iwinghire',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'usersDB', 
+#         'USER': 'sammy', 
+#         'PASSWORD': 'pa$$word',
+#         'HOST': '127.0.0.1', 
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
