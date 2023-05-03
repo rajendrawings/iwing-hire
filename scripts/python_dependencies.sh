9873eee821b726c02297a6dd9c37290d10da9992
rajendrawings <rajendra.badgujar@integrationwings.com>
2023-05-03 11:05:09 +0530

    Update pipeline

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -3,3 +3,12 @@
 virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
 pip install -r /home/ubuntu/iwinghire/requirements.txt
+
+cd /home/ubuntu/iwinghire
+sudo mkdir ./static
+python manage.py makemigrations 
+python manage.py migrate 
+python manage.py makemigrations     
+python manage.py collectstatic
+
+cd 
