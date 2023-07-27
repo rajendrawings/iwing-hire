bebce193b95f1a2063c60b2cd06746fc7e8aca74
rajendra <rajendra.badgujar@integrationwings.com>
2023-07-27 22:36:20 +0530

    added serializer for Comapanylogo and Subscription

--- company/serializers.py
+++ company/serializers.py
@@ -1,8 +1,26 @@
 from rest_framework import serializers
-from .models import Company
+from .models import Company, CompanyLogo, Subscription
+
+class CompanyLogoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CompanyLogo
+        fields = ["file", "company"]
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = ["plan", "amount"]
 
 
 class CompanySerializer(serializers.ModelSerializer):
+    file = serializers.FileField(required=False)
+    subscription = SubscriptionSerializer()
     class Meta:
         model = Company
-        fields = ["name"]
+        fields = ['name', 'subscription',  'file']
+
+
+
+
+
