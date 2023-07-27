ca8eb1d9ec59498bf62891e0e5ed7707924ccf19
rajendra <rajendra.badgujar@integrationwings.com>
2023-07-27 22:29:09 +0530

    register CompanyLogo and Subscription at admin site

--- company/admin.py
+++ company/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Company
+from .models import Company, CompanyLogo, Subscription
 
 admin.site.register(Company)
+admin.site.register(CompanyLogo)
+admin.site.register(Subscription)
+
