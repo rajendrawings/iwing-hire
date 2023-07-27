7c51595ba3795eb35d4191732d4ebd43a6def78d
rajendra <rajendra.badgujar@integrationwings.com>
2023-07-27 22:38:51 +0530

    added urls for Subscription

--- company/urls.py
+++ company/urls.py
@@ -3,8 +3,13 @@ from django.urls import path, include
 
 from .views import (
     CompanyListApiView,
+    SubscriptionListApiView,
+    SubscriptionDetailApiView
 )
 
 urlpatterns = [
     path('companys', CompanyListApiView.as_view()),
+    path('subscriptions', SubscriptionListApiView.as_view()),
+    path('subscription/<int:pk>', SubscriptionDetailApiView.as_view()),
+
 ]
