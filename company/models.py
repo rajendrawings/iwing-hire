356f72de4f2dd50cb199562e73dd0430f806ae46
rajendra <rajendra.badgujar@integrationwings.com>
2023-07-29 15:30:53 +0530

    added companysubascription model

--- company/models.py
+++ company/models.py
@@ -11,7 +11,7 @@ class Subscription(models.Model):
     )
 
     plan = models.CharField(max_length=20, choices=PLAN_CHOICES)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
@@ -19,25 +19,27 @@ class Subscription(models.Model):
 class Company(models.Model):
     name = models.CharField(max_length = 180, unique=True)
     user = models.ForeignKey(User, on_delete = models.CASCADE, blank = True, null = True)
-    # subscription = models.ForeignKey(Subscription, on_delete = models.CASCADE, blank = True, null = True)
     address = models.CharField(max_length = 500, blank = True, null = True)
     designation = models.CharField(max_length = 200, blank = True, null = True)
-    email = models.CharField(max_length=100, blank = True, null = True)
-    mobile_no = models.IntegerField(blank = True, null = True)
-    gst_no = models.IntegerField(blank = True, null = True)
+    email = models.EmailField(max_length=100, blank = True, null = True)
+    mobile_number = models.IntegerField(blank = True, null = True)
+    gst_number = models.IntegerField(blank = True, null = True)
     established_date = models.DateTimeField(timezone.now())
     about = models.CharField(max_length = 400, blank = True, null = True)
     terms_policy = models.CharField(max_length = 500, blank = True, null = True)
+    Company_logo = models.FileField(upload_to='media/images/activity', null=True)
+    created_at = models.DateTimeField(timezone.now())
+    modified_at = models.DateTimeField(timezone.now)
     
-
-
     def __str__(self):
         return self.name
 
 
-class CompanyLogo(models.Model):
-    file = models.FileField(upload_to = "media/images/activity", null=True)
-    company = models.ForeignKey(Company, on_delete = models.CASCADE, blank = True, null = True)
-
+class CompanySubscription(models.Model):
+    company = models.ForeignKey(Company, on_delete = models.CASCADE, blank=True, null= True)
+    subscription = models.ForeignKey(Subscription, on_delete = models.CASCADE, blank=True, null=True)
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
 
 
