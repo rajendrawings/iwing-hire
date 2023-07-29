b430fa4896b0b78220300a613ace47687442a16a
rajendra <rajendra.badgujar@integrationwings.com>
2023-07-29 15:34:24 +0530

    url for companysubscription

--- company/urls.py
+++ company/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path, include
-#from rest_framework.authtoken import views as authtoken_views
 
 from .views import (
     CompanyListApiView,
     SubscriptionListApiView,
-    SubscriptionDetailApiView
+    SubscriptionDetailApiView,
+    CompanySubscriptionListAPIView
 )
 
 urlpatterns = [
     path('companys', CompanyListApiView.as_view()),
     path('subscriptions', SubscriptionListApiView.as_view()),
     path('subscription/<int:pk>', SubscriptionDetailApiView.as_view()),
-
+    path('companysubscription', CompanySubscriptionListAPIView.as_view()),
 ]
