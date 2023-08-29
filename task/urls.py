92014bb7a5fe3e8b5856d44d4f86167140eb78db
rajendrawings <rajendra.badgujar@integrationwings.com>
2023-08-30 01:02:53 +0530

    Added comment in task url.py

--- task/urls.py
+++ task/urls.py
@@ -3,27 +3,27 @@ from .views import *
 
 
 urlpatterns = [
-#Below urls for Board
+    #Below urls for Board
     path('board', BoardApiView.as_view()),
     path('board/<int:pk>', BoardDetailApiView.as_view()),
 
-#Below urls for card
+    #Below urls for card
     path('card', CardApiView.as_view()),
     path('card/<int:pk>', CardDetailApiView.as_view()),
 
-#Below urls for Task
+    #Below urls for Task
     path('task', TaskApiView.as_view()),
     path('task/<int:pk>', TaskDetailApiView.as_view()),
 
-#Below urls for Activity
+    #Below urls for Activity
     path('activity', ActivityApiView.as_view()),
     path('activity/<int:pk>', ActivityDetailApiView.as_view()),
 
-#Below urls for Jobs
+    #Below urls for Jobs
     path('job', JobApiView.as_view()),
     path('job/<int:pk>', JobDetailApiView.as_view()),
 
-#Below urls for Interviewer
+    #Below urls for Interviewer
     path('interviewer', InterviewerApiView.as_view()),
     path('interviewer/<int:pk>', InterviewerDetailApiView.as_view()),
     path('import_interviewer',InterviewerImportAPI.as_view())
